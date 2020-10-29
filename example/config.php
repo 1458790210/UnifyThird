1abb2b460563b556f0e244ee1f227b2b4b140cea
ryan <123123cc>
2020-10-29 17:31:26 +0800

    add

--- example/config.php
+++ example/config.php
@@ -4,7 +4,6 @@ return [
         'app_id'        => '',
         'secret'        => '',
         'response_type' => 'array',
-
         'log' => [
             'level' => 'debug',
             'file'  => __DIR__ . '/wechat.log',
@@ -20,7 +19,23 @@ return [
         'format'             => 'json',
         'alipayPublicKey'    => '',
         'debugInfo'          => false,
-        'rsaPrivateKey'      => '',
-        'alipayrsaPublicKey' => '',
+        'rsaPrivateKey'      => '你的应用私钥',
+        'alipayrsaPublicKey' => '你的支付宝公钥',
+    ],
+    'Alipay_cert' => [
+        'gatewayUrl'         => 'https://openapi.alipay.com/gateway.do',
+        'appId'              => '',
+        'apiVersion'         => '1.0',
+        'signType'           => 'RSA2',
+        'postCharset'        => 'utf-8',
+        'encryptKey'         => '',
+        'format'             => 'json',
+        'alipayPublicKey'    => '',
+        'debugInfo'          => false,
+        'rsaPrivateKey'      => '你的应用私钥',
+        'alipayrsaPublicKey' => '你的支付宝公钥',
+        'appCertPath'        => "应用证书路径（要确保证书文件可读），例如：/home/admin/cert/appCertPublicKey.crt",
+        'alipayCertPath'     => "支付宝公钥证书路径（要确保证书文件可读），例如：/home/admin/cert/alipayCertPublicKey_RSA2.crt",
+        'rootCertPath'       => "支付宝根证书路径（要确保证书文件可读），例如：/home/admin/cert/alipayRootCert.crt",
     ]
 ];
--- src/UnifyThird/Alipay/AlipayCert.php
+++ src/UnifyThird/Alipay/AlipayCert.php
@@ -13,6 +13,14 @@ class AlipayCert extends Strategy
         echo "单例模式的AlipayCert被构造了" . PHP_EOL;
         $aop = new \AopCertClient();
         $this->AliPayConstruct($aop);
+        //调用getPublicKey从支付宝公钥证书中提取公钥
+        $this->app->alipayrsaPublicKey = $this->app->getPublicKey(self::$init['alipayCertPath']);
+        //是否校验自动下载的支付宝公钥证书，如果开启校验要保证支付宝根证书在有效期内
+        $this->app->isCheckAlipayPublicCert = true;
+        //调用getCertSN获取证书序列号
+        $this->app->appCertSN = $this->app->getCertSN(self::$init['appCertPath']);
+        //调用getRootCertSN获取支付宝根证书序列号
+        $this->app->alipayRootCertSN = $this->app->getRootCertSN(self::$init['rootCertPath']);
     }
 
     public function __call($name, $arguments)
